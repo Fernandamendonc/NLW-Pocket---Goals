2cdfe5a989f7e779edc3a06a797bda10906d5b47
Fernandamendonc <fernandamendoncadepinho@gmail.com>
2025-02-20 23:32:26 -0300

    goals route creation

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,21 @@
 import fastify from "fastify";
+import z from "zod";
+import { createGoals } from "../functions/create-goals";
 
 const app = fastify();
+
+app.post("/goals", async (request) => {
+  const createGoalsSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  });
+  const body = createGoalsSchema.parse(request.body);
+
+  await createGoals({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  });
+});
 
 app
   .listen({
